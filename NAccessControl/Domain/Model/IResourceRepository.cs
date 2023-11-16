f29ee7d00999484a3e68df2cc8b1b6c3d753d265
CJ <weicongju@gmail.com>
2023-11-16 13:43:54 +0800

    接口更新

--- NAccessControl/Domain/Model/IResourceRepository.cs
+++ NAccessControl/Domain/Model/IResourceRepository.cs
@@ -12,4 +12,8 @@ public interface IResourceRepository
     Task<IEnumerable<Resource>> FindAllResourcesAsync();
 
     public IEnumerable<OwnedResource> FindOwnedResources(IUserId user);
+
+    Task<IEnumerable<OwnedResource>> FindOwnedResourcesAsync(Role role, string parentKey);
+
+    Task<IEnumerable<OwnedResource>> FindAllOwnedResourcesAsync(Role role);
 }
--- NAccessControl/Domain/Model/OwnedResource.cs
+++ NAccessControl/Domain/Model/OwnedResource.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace NAccessControl.Domain.Model;
+﻿namespace NAccessControl.Domain.Model;
 
 public class OwnedResource
 {
     public int Id { get; set; }
+
+    public Role Role { get; set; }
+
+    public int RoleId { get; set; }
 
     public UserId? UserId { get; set; }
 
