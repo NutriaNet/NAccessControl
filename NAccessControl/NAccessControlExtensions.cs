835151c855a3fb90219f5919a2c91e4d09bfc0fc
zhouzhifeng <1354061659@qq.com>
2023-12-11 05:08:11 +0800

    add NAccessControl Extensions to services

---
+++ NAccessControl/NAccessControlExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NAccessControl.Interfaces.Facade;
+using NAccessControl.Interfaces.Facade.Assembler;
+using NAccessControl.Interfaces.Facade.Impl;
+using NAccessControl.Service;
+using NAccessControl.Service.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAccessControl
+{
+    public static class NAccessControlExtensions
+    {
+        public static IServiceCollection AddNAccessControl(this IServiceCollection services)
+        {
+            services.AddScoped<IAccessControlService, AccessControlService>(); 
+            services.AddScoped<IAccessControlServiceFacade, AccessControlServiceFacade>();
+            services.AddScoped<PermissionAssember>();
+            services.AddScoped<ResourceAssember>();
+            return services;
+        }
+    }
+}
